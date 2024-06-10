b9e062b09b4bf1c10886c7c86602936ca4ff2e05
chaekie <chaekie.kim@gmail.com>
2024-06-10 17:45:37 +0900

    set up config

--- SmokingAreaMap/ContentView.swift
+++ SmokingAreaMap/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let env = ProcessInfo.processInfo.environment
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
