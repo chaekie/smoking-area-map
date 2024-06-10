426a832a49f884a7ef2d5d007387fa9c4ac0bf9c
chaekie <chaekie.kim@gmail.com>
2024-06-10 15:24:05 +0900

    init project

---
+++ SmokingAreaMap/SmokingAreaMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmokingAreaMapApp.swift
+//  SmokingAreaMap
+//
+//  Created by chaekie on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SmokingAreaMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
